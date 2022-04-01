c8e05fe9aaa046bfc14645778a2d6005b1e813a6
Sebbooo <51121423+RealSebbooo@users.noreply.github.com>
2022-04-01 17:00:47 +0200

    add babelrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "./../components/layout";
 import Title from "./../components/title";
 import AutocompleteField from "./../components/autocomplete";
 
 import ItemList from "./../components/itemList";
-import { ShoppingListType } from "../types";
-import { getShoppingList } from "../services/databaseHelper";
 
 const Index = () => {
   return (
