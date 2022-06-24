78618296c18d4c7ea81b733f442a4c045cad0815
Sebastian Koch <s.koch@alexander-buerkle.de>
2022-06-24 10:58:55 +0200

    adding ssr function

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { isLoggedIn } from "./src/services/auth";
+import { isLoggedIn, showFooter } from "./src/services/auth";
 import Container from "./src/components/container";
 import FooterNav from "./src/components/footer";
 import "./src/styles/index.css";
